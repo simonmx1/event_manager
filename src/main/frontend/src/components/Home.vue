efb20f215b71aa090cd840a92dbdb1b68c4f50e3
simonmx1 <simon@muscatello.bz.it>
2021-12-20 09:42:25 +0100

    Working User Authentication & CORS

--- src/main/frontend/src/components/Home.vue
+++ src/main/frontend/src/components/Home.vue
@@ -1,10 +1,28 @@
 <template>
-  <h1>Welcome Home</h1>
+  <v-app>
+    <v-app-bar clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
+      <v-app-bar-title>
+        Event Manager
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer clipped>
+
+    </v-navigation-drawer>
+    <v-container>
+      <user-management/>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import UserManagement from "@/components/UserManagement";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {UserManagement},
+  data: () => ({
+    drawer: false
+  })
 }
 </script>
 
---
+++ src/main/frontend/src/components/UserManagement.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-main>
+
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "UserManagement"
+}
+</script>
+
+<style scoped>
+
+</style>
