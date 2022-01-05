56175072a065b4e406422b74e9a9149926c88a0d
simonmx1 <simon@muscatello.bz.it>
2022-01-05 16:36:10 +0100

    Set up navigation guard and blocked requests for non-authorized users or roles

--- src/main/frontend/src/utils/api.js
+++ src/main/frontend/src/utils/api.js
@@ -42,7 +42,6 @@ export default {
         }).catch(() => false);
     },
     async loggedIn() {
-        let jwt;
         if (JSON.parse(localStorage.getItem('jwt')) == null) {
             return ""
         } else  {
