549a03345c72482182d52c8a6ec8ddf1aa858bcc
Manuel Reichegger <manuel.reichegger@student.uibk.ac.at>
2022-01-20 18:29:03 +0100

    add index to timeslots in PollForm.vue

--- src/main/frontend/src/components/PollForm.vue
+++ src/main/frontend/src/components/PollForm.vue
@@ -35,22 +35,43 @@
         Sort your Timeslots:
       </v-card-title>
       <draggable v-model="timeslots" group="people" @start="drag=true" @end="drag=false">
-        <div v-for="(item) in timeslots"
+        <div v-for="(item, index) in timeslots"
              :key="item.timeslot.id"
         >
+
+          <tr>
+            <th>
+              <v-chip>{{ index + 1 }}</v-chip>
+            </th>
+            <th style="width: 100%">
               <v-card color="#3d3d3d" class="pa-2, ma-2">
-                  <v-card-subtitle style="font-size: medium; color: white">
-                    From:
-                    <div style="float: right">
-                      {{ formatTimeStamp(item.timeslot.start).date }} at {{ formatTimeStamp(item.timeslot.start).time }}
-                    </div>
-                    <br>
-                    To:
-                    <div style="float: right">
-                      {{ formatTimeStamp(item.timeslot.end).date }} at {{ formatTimeStamp(item.timeslot.end).time }}
-                    </div>
-                  </v-card-subtitle>
+                <v-card-subtitle style="font-size: medium; color: white">
+                  <v-row>
+                    <v-col cols="2">
+                      <div style="float: left">
+                        From:
+                      </div>
+                      <br>
+                      <div style="float: left">
+                        To:
+                      </div>
+                    </v-col>
+                    <v-col cols="8">
+                      <div style="float: right">
+                        {{ formatTimeStamp(item.timeslot.start).date }} at {{ formatTimeStamp(item.timeslot.start).time }}
+                      </div>
+                      <div style="float: right">
+                        {{ formatTimeStamp(item.timeslot.end).date }} at {{ formatTimeStamp(item.timeslot.end).time }}
+                      </div>
+                    </v-col>
+                    <v-col cols="1">
+                      <v-icon color="#aaa" style="height: 100%; width: 50px">mdi-reorder-horizontal</v-icon>
+                    </v-col>
+                  </v-row>
+                </v-card-subtitle>
               </v-card>
+            </th>
+          </tr>
         </div>
       </draggable>
     </v-col>
