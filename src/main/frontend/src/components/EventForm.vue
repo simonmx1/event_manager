0027eb23bd374aa35f0f70beda00f189661e06a4
simonmx1 <simon@muscatello.bz.it>
2022-01-23 15:40:32 +0100

    Timeslots are required now in create EventForm.vue

--- src/main/frontend/src/components/EventForm.vue
+++ src/main/frontend/src/components/EventForm.vue
@@ -28,14 +28,29 @@
           <v-row v-for="(timeslot, index) in currentEvent.timeslots" :key="index" ref="timeslots">
             <v-col class=".col-auto">
               <v-datetime-picker
-                  :time-picker-props="{format:'24hr', allowedMinutes:allowedStepTimeSlot,}"
-                  :date-picker-props="{min: new Date().toISOString()}"
+                  :time-picker-props="{format:'24hr', allowedMinutes:allowedStepTimeSlot, max: timeslot.end != null ? timeslot.end.getTime().toString() : null}"
+                  :date-picker-props="{min: new Date().toISOString(), max: timeslot.end != null ? timeslot.end.toISOString() : null}"
                   label="Select Starttime"
-                  v-model="timeslot.start"></v-datetime-picker>
+                  v-model="timeslot.start">
+                <template v-slot:dateIcon>
+                  <v-icon>mdi-calendar</v-icon>
+                </template>
+                <template v-slot:timeIcon>
+                  <v-icon>mdi-clock</v-icon>
+                </template>
+              </v-datetime-picker>
             </v-col>
             <v-col class=".col-auto">
               <v-datetime-picker :time-picker-props="{format:'24hr', allowedMinutes:allowedStepTimeSlot}"
-                                 label="Select Endtime" v-model="timeslot.end"></v-datetime-picker>
+                                 :date-picker-props="{min: getMinEndDate(timeslot) != null ? getMinEndDate(timeslot) : new Date().toISOString()}"
+                                 label="Select Endtime" v-model="timeslot.end">
+                <template v-slot:dateIcon>
+                  <v-icon>mdi-calendar</v-icon>
+                </template>
+                <template v-slot:timeIcon>
+                  <v-icon>mdi-clock</v-icon>
+                </template>
+              </v-datetime-picker>
             </v-col>
             <v-col v-if="index + 1 === currentEvent.timeslots.length" cols="auto" style="margin-top: 10px">
               <v-btn v-if="currentEvent.timeslots.length > 1" icon @click="removeTimeslotInput(index)">
@@ -51,14 +66,29 @@
               </v-btn>
             </v-col>
           </v-row>
+          <v-alert
+              v-if="timeslotError != null"
+              dense
+              outlined
+              type="error"
+          >
+            <strong>{{ timeslotError }}</strong>
+          </v-alert>
         </div>
         <v-divider/>
         <v-datetime-picker
             :time-picker-props="{format:'24hr', allowedMinutes:allowedStepTimeEnd}"
-            :date-picker-props="{min: new Date().toISOString(), max: getFirstTimeslotStart()}"
+            :date-picker-props="{min: new Date().toISOString(), max: pollEndDateMax.date}"
             label="Poll end time"
             v-model="currentEvent.pollEndDate"
-            :text-field-props="{errorMessages: pollEndDateError}"/>
+            :text-field-props="{errorMessages: pollEndDateError}">
+          <template v-slot:dateIcon>
+            <v-icon>mdi-calendar</v-icon>
+          </template>
+          <template v-slot:timeIcon>
+            <v-icon>mdi-clock</v-icon>
+          </template>
+        </v-datetime-picker>
       </v-col>
     </v-row>
   </v-form>
@@ -100,13 +130,24 @@ export default {
   }),
   methods: {
     getFirstTimeslotStart() {
-      let first = null
+      let first = this.currentEvent.timeslots[0]
       this.currentEvent.timeslots.forEach(timeslot => {
-        if (first != null && first) {
-          console.log(timeslot)
+        if (first.start <= timeslot.start) {
+          first = timeslot
         }
       })
-      return first
+      return {
+        date: first.start != null ? this.addOneHour(first.start).toISOString() : null,
+        time: first.start != null ? first.start.getTime() : null
+      }
+    },
+    getMinEndDate(timeslot) {
+      return timeslot.start != null ? this.addOneHour(timeslot.start).toISOString() : null
+    },
+    addOneHour(timestamp) {
+      let date = new Date(timestamp)
+      date.setHours(date.getHours() + 1)
+      return date
     },
     addTimeslotInput() {
       this.currentEvent.timeslots.push({start: null, end: null})
@@ -160,26 +201,29 @@ export default {
       } else {
         this.eventNameError = null
       }
-      //this.currentEvent.timeslots.forEach(timeslot => )
       if (this.currentEvent.pollEndDate == null) {
         this.pollEndDateError = 'Please enter a end date for the poll of this event'
         valid = false
       } else {
         this.pollEndDateError = null
       }
-      if (this.currentEvent.pollEndDate == null) {
-        console.log("pollenddate: ", this.currentEvent.pollEndDate);
-        this.pollEndDateError = 'Please enter a end date for the poll of this event'
-        valid = false
-      } else {
-        this.pollEndDateError = null
-      }
+      this.currentEvent.timeslots.forEach(timeslot => {
+        if (timeslot.start == null || timeslot.end == null) {
+          this.timeslotError = "Some of your timeslots are invalid"
+          valid = false
+        } else {
+          this.timeslotError = null
+        }
+      })
       if (valid)
         this.$emit('confirm', this.currentEvent)
     },
     clear() {
       this.participantsError = null
       this.locationsError = null
+      this.pollEndDateError = null
+      this.timeslotError = null
+      this.eventNameError = null
       this.$refs.form.reset()
       this.currentEvent = JSON.parse(JSON.stringify(this.event))
       this.$forceUpdate()
@@ -188,6 +232,19 @@ export default {
   mounted() {
     this.currentEvent = JSON.parse(JSON.stringify(this.event))
     this.$api.user.getAll().then(response => this.availableUsers = response)
+  },
+  computed: {
+    pollEndDateMax: function () {
+      return this.getFirstTimeslotStart()
+    }
+  },
+  watch: {
+    '$data.currentEvent.timeslots': {
+      handler: function (val) {
+        console.log(val)
+      },
+      deep: true
+    }
   }
 }
 </script>
