8fa2010415f5bac5faca6009794ee2e344a87048
Manuel Reichegger <manuel.reichegger@student.uibk.ac.at>
2022-01-23 11:54:31 +0100

    added PollResultDialog.vue

--- src/main/frontend/src/utils/api.js
+++ src/main/frontend/src/utils/api.js
@@ -275,7 +275,6 @@ export default {
             return await axios.get('/api/poll/get?eventId=' + eventId + '&username=' + username,
                 {headers: {"Authorization": "Bearer " + JSON.parse(localStorage.getItem('jwt'))}}
             ).then(response => {
-                console.log(response)
                 return response.data;
             }).catch(() => false);
         },
