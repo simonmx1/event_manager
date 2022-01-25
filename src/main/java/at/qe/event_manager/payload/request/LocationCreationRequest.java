f1d1402a8b8920a024d4eb2d86f069013c226e94
Matthias Komar <matthias.komar@student.uibk.ac.at>
2022-01-25 19:11:25 +0100

    deleted setter from LocationCreationRequest.java

--- src/main/java/at/qe/event_manager/payload/request/LocationCreationRequest.java
+++ src/main/java/at/qe/event_manager/payload/request/LocationCreationRequest.java
@@ -63,71 +63,35 @@ public class LocationCreationRequest {
         return locationId;
     }
 
-    public void setLocationId(Integer locationId) {
-        this.locationId = locationId;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getMenu() {
         return menu;
     }
 
-    public void setMenu(String menu) {
-        this.menu = menu;
-    }
-
     public Date getCreateDate() {
         return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
     }
 
     public String getGeolocation() {
         return geolocation;
     }
 
-    public void setGeolocation(String geolocation) {
-        this.geolocation = geolocation;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public boolean isEnabled() {
         return enabled;
     }
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
     public Set<Tag> getTags() {
         return tags;
     }
 
-    public void setTags(Set<Tag> tags) {
-        this.tags = tags;
-    }
-
     public List<OpeningTime> getOpeningTimes() {
         return openingTimes;
-    }
-
-    public void setOpeningTimes(List<OpeningTime> openingTimes) {
-        this.openingTimes = openingTimes;
     }
 }
